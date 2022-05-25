6d28ee10fdf8e461c8f98b3749d70b93b9f8a0eb
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2022-05-25 09:58:19 +0530

    testing file io

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,12 +1,37 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+const filename string = "my_cards"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-	print("Length is ", len(d))
-	if len(d) != 53 {
-		t.Errorf("Expected length of 53, but got %v", len(d))
+
+	if len(d) != 52 {
+		t.Errorf("Expected length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Joker" {
+		t.Errorf("Expected Joker but got %v", d[0])
+	}
+
+}
+
+func TestSaveToFile(t *testing.T) {
+	os.Remove(filename)
+
+	d := newDeck()
+	d.saveToFile(filename)
+
+	loadedDeck := readDeckFromFile(filename)
+
+	if len(loadedDeck) != 53 {
+		t.Errorf("Expected 53 cards in deck but got %v", len(loadedDeck))
+	}
+
+	os.Remove(filename)
 
 }
